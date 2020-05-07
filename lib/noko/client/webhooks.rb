f40b0f40bf300bab0ddf522bd7ef11132f49b780
Rebecca Vetter <rebecca@radialdevgroup.com>
2020-05-07 15:40:55 -0600

    Create and get webhook

--- lib/noko/client/webhooks.rb
+++ lib/noko/client/webhooks.rb
@@ -3,5 +3,13 @@ module Noko
     def get_webhooks(params = nil)
       get('/v2/webhooks', params)
     end
+
+    def get_webhook(id)
+      get("/v2/webhooks/#{id}")
+    end
+
+    def create_webhook(attributes)
+      post('/v2/webhooks', attributes)
+    end
   end
 end
--- test/noko/client/webhooks_test.rb
+++ test/noko/client/webhooks_test.rb
@@ -14,13 +14,29 @@ class ClientWebhooksTest < ClientTest
   end
 
   def test_get_webhooks_encodes_event_params
+    skip("test this after I make a webhook")
     expect_request(:get, "#{base_url}/webhooks?events=entry.updated,entry.updated.approved")
 
     client.get_webhooks(events: "entry.updated, entry.updated.approved")
   end
 
-  # def test_get_webhook
-  # def test_create_webhook
+  def test_get_webhooks_encodes_state_params
+    expect_request(:get, "#{base_url}/webhooks?state=disabled")
+
+    client.get_webhooks(state: "disabled")
+  end
+
+  def test_get_webhook
+    expect_request(:get, "#{base_url}/webhooks/#{id}").with(auth_header).to_return(json_response)
+
+    assert_instance_of Noko::Record, client.get_webhook(id)
+  end
+
+  def test_create_webhook
+    expect_request(:post, "#{base_url}/webhooks").with(json_request).to_return(json_response.merge(status: 201))
+
+    assert_instance_of Noko::Record, client.create_webhook(name: "My Webhook", payload_uri: "http://testnokoapp.com/webhooks/entry_events", events: ["*"])
+  end
   # def test_update_webhook
   # def test_add_webhook_events
   # def test_delete_webhook_events
